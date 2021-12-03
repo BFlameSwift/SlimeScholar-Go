cc93a9aff80bcefe7e4f0b48505c1634968bec7f
BFlameSwift <19373732@buaa.edu.cn>
2021-12-03 20:28:35 +0800

    something browse

--- service/my_elasticsearch.go
+++ service/my_elasticsearch.go
@@ -337,7 +337,7 @@ func IdsGetItems(id_list []string, index string) map[string]interface{} {
 
 func SimplifyPaper(m map[string]interface{}) map[string]interface{} {
 	var ret map[string]interface{} = make(map[string]interface{})
-	ret["id"], ret["authors"], ret["citation_num"], ret["journalName"], ret["paperAbstract"], ret["reference_num"], ret["year"], ret["title"] = m["id"], m["authors"], m["citation_num"], m["journalName"], m["paperAbstract"], m["reference_num"], m["year"], m["title"]
+	ret["id"], ret["authors"], ret["citation_count"], ret["journalName"], ret["paperAbstract"], ret["reference_count"], ret["year"], ret["title"] = m["id"], m["authors"], m["citation_num"], m["journalName"], m["paperAbstract"], m["reference_num"], m["year"], m["title"]
 	return ret
 }
 
@@ -382,7 +382,7 @@ func PaperGetAuthors(paper_id string) map[string]interface{} {
 	}
 	return paper_reference_rel_map
 }
-func PaperRelMakeMap(str string) ([]interface{}) {
+func PaperRelMakeMap(str string) []interface{} {
 	ret_map := make(map[string]interface{})
 	err := json.Unmarshal([]byte(str), &ret_map)
 	if err != nil {
