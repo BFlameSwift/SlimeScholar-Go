7b59e7ddc422e755f4f9233f6da0caf3c18a5d7c
echo0120 <echo0120@126.com>
2021-12-06 21:57:33 +0800

    .

--- docs/docs.go
+++ docs/docs.go
@@ -1012,7 +1012,7 @@ var doc = `{
                             "type": "string"
                         }
                     },
-                    "403": {
+                    "400": {
                         "description": "{\"success\": false, \"message\": \"回复不存在\"}",
                         "schema": {
                             "type": "string"
