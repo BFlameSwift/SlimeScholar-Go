59bc4711793476a72e1d17502d1b76d90fd004b3
Matrix53 <1079207272@qq.com>
2021-12-14 00:59:07 +0800

    chore: database url

--- utils/secret.go
+++ utils/secret.go
@@ -8,7 +8,7 @@ const HUAWEI_ELASTIC_SEARCH_HOST = "http://124.70.95.61:9200"
 
 //const ELASTIC_SEARCH_HOST = "http://4r244274i3.zicp.vip:23656"
 
-const LOCAL_ELASTIC_SEARCH_HOST = "http://127.0.0.1:9200"
+const LOCAL_ELASTIC_SEARCH_HOST = "http://124.70.95.61:9200"
 
 const ELASTIC_SEARCH_HOST = LOCAL_ELASTIC_SEARCH_HOST
 
@@ -16,6 +16,6 @@ const FRONTEND_URL = "https://slime.matrix53.top"
 
 //const REDIS_HOST = "124.70.95.61:6379"
 
-const REDIS_HOST = "127.0.0.1:6379"
+const REDIS_HOST = "124.70.95.61:6379"
 
 const REDIS_PASSWORD = "redis1921@"
